286bd9b551689b795af686a06bd8bb54bf3a27f2
秋のかえで <autmaple@protonmail.com>
2021-11-14 12:51:29 +0800

    feat: remove unused info function

--- naruse.go
+++ naruse.go
@@ -1,7 +1,5 @@
 package naruse
 
-import "runtime"
-
 const (
 	version = "0.1.0"
 	usage   = "A relay for VMess."
@@ -13,9 +11,4 @@ func Version() string {
 
 func Usage() string {
 	return usage
-}
-
-func Info() string {
-	return "Naruse " + Version() + " (" + Usage() + ") (" + runtime.Version() + " " +
-		runtime.GOOS + "/" + runtime.GOARCH + ")"
 }
